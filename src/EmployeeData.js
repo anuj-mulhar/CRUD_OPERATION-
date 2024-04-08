888ccd30bfea179197c9df286a25c0c41a2c6e18
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-08 10:35:46 +0530

    CRUD operation

---
+++ src/EmployeeData.js
@@ -0,0 +1,26 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName : "Kunal ",
+        lastName: "Nakum",
+        age : 20
+    },
+    {
+        id: 2,
+        firstName : "Aditya ",
+        lastName: "Patel",
+        age : 22
+    },
+    {
+        id: 3,
+        firstName : "Hiren ",
+        lastName: "Javiya",
+        age : 18
+    },
+    {
+        id: 4,
+        firstName : "Vicky ",
+        lastName: "Parmar",
+        age : 20
+    }
+]
